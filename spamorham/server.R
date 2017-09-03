b3a95ad6e4c1e7e7be0924ccbda2904979d85ef0
Ansih Singh Walia <loveeanish@gmail.com>
2017-09-03 23:38:48 +0530

    finally made the classifier web app

--- spamorham/server.R
+++ spamorham/server.R
@@ -8,13 +8,50 @@
 #
 
 library(shiny)
+library(quanteda)
 
-# Define server logic required to draw a histogram
+
+#R-code for the 
+spam<-read.csv("F:/PROJECTS/Datasets/spam.csv",header=TRUE, sep=",", quote='\"\"', stringsAsFactors=FALSE)
+set.seed(2012)
+spam<-spam[sample(nrow(spam)),]
+
+names(spam)<-c("type","message")
+
+#to search more on this method
+msg.corpus<-corpus(spam$message)
+docvars(msg.corpus)<-spam$type   #ataching the label to the corpus message text
+spam.train<-spam[1:4458,]
+
+
+msg.dfm <- dfm(msg.corpus, tolower = TRUE)  #generating document freq matrix
+msg.dfm <- dfm_trim(msg.dfm, min_count = 5, min_docfreq = 3)  
+msg.dfm <- dfm_weight(msg.dfm, type = "tfidf") 
+
+#trining and testing data of dfm 
+msg.dfm.train<-msg.dfm[1:4458,]
+
+#training the naive-bayes classifier
+nb.classifier<-textmodel_NB(msg.dfm.train,spam.train[,1])
+
+
+
 shinyServer(function(input, output) {
   
+ 
   
-  output$label <- renderText({input$message
-                              
+ 
+  
+  
+  output$label <-renderText({
+    msg=corpus(input$message)
+    ms.dfm<-dfm(msg,tolower=TRUE,stem=TRUE, remove=stopwords("english"))
+    ms.dfm=dfm_select(ms.dfm,msg.dfm.train)
+    #predicting on the entered text by user
+    class=predict(nb.classifier,newdata=ms.dfm)
+    class$nb.predicted
+    
+  
   })
   
   
